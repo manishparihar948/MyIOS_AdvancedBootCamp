c3606baf368c6a292d584a50c622ec3db9221b88
manishparihar948 <manish.parihar948@gmail.com>
2023-08-31 14:04:11 +0200

    Background Materials

---
+++ SwiftAdvancedLearning/BackgroundMaterialsBootcamp.swift
@@ -0,0 +1,36 @@
+//
+//  BackgroundMaterialsBootcamp.swift
+//  SwiftAdvancedLearning
+//
+//  Created by Manish Parihar on 31.08.23.
+//
+
+import SwiftUI
+
+struct BackgroundMaterialsBootcamp: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack {
+                RoundedRectangle(cornerRadius: 4)
+                    .frame(width: 50, height: 4)
+                    .padding()
+                Spacer()
+            }
+            .frame(height:350)
+            .frame(maxWidth: .infinity)
+            .background(.ultraThinMaterial)
+            .cornerRadius(30)
+        }
+        .ignoresSafeArea()
+        .background(
+            Image("manish")
+        )
+    }
+}
+
+struct BackgroundMaterialsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundMaterialsBootcamp()
+    }
+}
